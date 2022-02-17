cc2fd6136fca0557e4e76b4de44e1cd2f433df53
Ilya Chistyakov <ichistyakovv@gmail.com>
2022-02-17 22:57:54 +0500

    feat: add path helper spec

---
+++ test/helpers/PathHelper.spec.ts
@@ -0,0 +1,27 @@
+import PathHelper from '../../src/helpers/PathHelper';
+
+describe('Path helper', () => {
+    it('Removes trailing slash', () => {
+        const result = PathHelper.removeTrailingSlash('/test/path/');
+
+        expect(result).toBe('/test/path');
+    });
+
+    it('Does not changes path if there is no trailing slash', () => {
+        const result = PathHelper.removeTrailingSlash('/test/path');
+
+        expect(result).toBe('/test/path');
+    });
+
+    it('Removes double slashes', () => {
+        const result = PathHelper.cleanPath('//some/test//path//');
+
+        expect(result).toBe('/some/test/path/');
+    });
+
+    it('Joins two strings into a path', () => {
+        const result = PathHelper.join('first', 'second');
+
+        expect(result).toBe('first/second');
+    });
+});
