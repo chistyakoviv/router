a61bb50604ae099e6c3d2d0b3f9fbfc8fd6bd097
Ilya Chistyakov <ichistyakovv@gmail.com>
2022-02-19 16:44:28 +0500

    refactor: replace function type with lamda

--- src/Resolver.ts
+++ src/Resolver.ts
@@ -3,14 +3,16 @@ import ResolverInterface from './interfaces/ResolverInterface';
 import Cache from './Cache';
 
 export default class Resolver implements ResolverInterface {
-    private cache: Cache<Function> = new Cache<Function>();
+    private cache: Cache<(...args: any) => string> = new Cache<
+        (...args: any) => string
+    >();
 
     public resolve(path: string, params?: object): string {
         try {
             const filler = this.cache.get(path, () => compile(path));
 
             return filler(params || {}, { pretty: true });
-        } catch (e) {
+        } catch (e: any) {
             throw new Error(`Missing param for route ${path}: ${e.message}`);
         }
     }
